fa3f989a0391afa46987f26acef7efc4b722c2e6
Maarten Paauw <maartenpaauw@gmail.com>
2016-12-01 21:16:20 +0100

    Opdracht 4

--- opdrachten/meubels/assets/js/main.js
+++ opdrachten/meubels/assets/js/main.js
@@ -2,7 +2,7 @@
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
-  for (var i = 0; i < 6; i++ ) {
+  for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
@@ -13,50 +13,58 @@ $(document).ready(function () {
 
   // Variables van alle muren.
   var muur01 = document.getElementById('muur01'),
-      muur02 = document.getElementById('muur02'),
-      muur03 = document.getElementById('muur03'),
-      muur04 = document.getElementById('muur04'),
+    muur02 = document.getElementById('muur02'),
+    muur03 = document.getElementById('muur03'),
+    muur04 = document.getElementById('muur04'),
 
-      // Meubels klasse
-      kruk = document.getElementsByClassName('kruk'),
-      stoel = document.getElementsByClassName('stoel'),
-      bank = document.getElementsByClassName('bank'),
+    // Meubels klasse
+    kruk = document.getElementsByClassName('kruk'),
+    stoel = document.getElementsByClassName('stoel'),
+    bank = document.getElementsByClassName('bank'),
 
-      // Meubels aan de muur ID
-      krukID = document.getElementById('kruk'),
-      stoelID = document.getElementById('stoel'),
-      bankID = document.getElementById('bank'),
+    // Meubels aan de muur ID
+    krukID = document.getElementById('kruk'),
+    stoelID = document.getElementById('stoel'),
+    bankID = document.getElementById('bank'),
 
-      // Meubels in de camera ID
-      krukCursorID = document.getElementById('krukCursorID'),
-      stoelCursorID = document.getElementById('stoelCursorID'),
-      bankCursorID = document.getElementById('bankCursorID'),
+    // Meubels in de camera ID
+    krukCursorID = document.getElementById('krukCursorID'),
+    stoelCursorID = document.getElementById('stoelCursorID'),
+    bankCursorID = document.getElementById('bankCursorID'),
 
-      // Meubels in bankHolder1
-      krukID01 = document.getElementById('kruk01'),
-      stoelID01 = document.getElementById('stoel01'),
-      bankID01 = document.getElementById('bank01'),
-      // Meubels in bankHolder2
-      krukID02 = document.getElementById('kruk02'),
-      stoelID02 = document.getElementById('stoel02'),
-      bankID02 = document.getElementById('bank02'),
-      // Meubels in bankHolder3
-      krukID03 = document.getElementById('kruk03'),
-      stoelID03 = document.getElementById('stoel03'),
-      bankID03 = document.getElementById('bank03'),
+    // Meubels in bankHolder1
+    krukID01 = document.getElementById('kruk01'),
+    stoelID01 = document.getElementById('stoel01'),
+    bankID01 = document.getElementById('bank01'),
 
-      // Bank holders
-      bank1Holder = document.getElementById('bank1Holder'),
-      bank2Holder = document.getElementById('bank2Holder'),
-      bank3Holder = document.getElementById('bank3Holder'),
+    // Meubels in bankHolder2
+    krukID02 = document.getElementById('kruk02'),
+    stoelID02 = document.getElementById('stoel02'),
+    bankID02 = document.getElementById('bank02'),
 
-      random = document.getElementById('randomBehang'),
-      randomKruk = document.getElementById('randomKruk'),
-      randomStoel = document.getElementById('randomStoel'),
-      randomBank = document.getElementById('randomBank');
+    // Meubels in bankHolder3
+    krukID03 = document.getElementById('kruk03'),
+    stoelID03 = document.getElementById('stoel03'),
+    bankID03 = document.getElementById('bank03'),
+
+    // Bank holders
+    bank1Holder = document.getElementById('bank1Holder'),
+    bank2Holder = document.getElementById('bank2Holder'),
+    bank3Holder = document.getElementById('bank3Holder'),
+
+    random = document.getElementById('randomBehang'),
+    randomKruk = document.getElementById('randomKruk'),
+    randomStoel = document.getElementById('randomStoel'),
+    randomBank = document.getElementById('randomBank');
+
+  // Het product in de cursor
+  var in_cursor = "";
+
+  // De producten niet in de cursor.
+  var not_in_cursor = [];
 
   // Voeg een event listener toe aan de random kleur kiezer.
-  random.addEventListener('mouseenter', function() {
+  random.addEventListener('mouseenter', function () {
 
     // Verkrijg een random kleur.
     var color = getRandomColor();
@@ -71,7 +79,7 @@ $(document).ready(function () {
 
   // KRUK
   // Voeg een event listener toe aan de random kleur kiezer.
-  randomKruk.addEventListener('mouseenter', function() {
+  randomKruk.addEventListener('mouseenter', function () {
 
     // Verkrijg een random kleur.
     var color = getRandomColor();
@@ -80,39 +88,93 @@ $(document).ready(function () {
     $(kruk).attr('material', 'color:' + color);
   });
 
-  krukID.addEventListener('mouseenter', function() {
+  krukID.addEventListener('mouseenter', function () {
 
     //Maak krukCursor visible
     $(krukCursorID).attr('visible', 'true');
     $(stoelCursorID).attr('visible', 'false');
     $(bankCursorID).attr('visible', 'false');
+
+    in_cursor = "kruk";
+
+    not_in_cursor = ["stoel", "bank"];
   });
 
-  bank1Holder.addEventListener('mouseenter', function() {
+  bank1Holder.addEventListener('mouseenter', function () {
 
-    //Maak kruk01 visible
-    $(krukID01).attr('visible', 'true');
+    if (in_cursor != "") {
+
+      // Maak het voorwerp zichbaar.
+      $('#' + in_cursor + '01').attr('visible', 'true');
+
+      // Haal het voorwerp uit de cursor.
+      $('#' + in_cursor + 'CursorID').attr('visible', 'false');
+
+      // Zet het in cursor variable opnieuw.
+      in_cursor = "";
+
+      $(not_in_cursor).each(function (key, value) {
+
+        // Maak het voorwerp onzichbaar.
+        $('#' + value + '01').attr('visible', 'false');
+      });
+
+      // Leeg de lijst.
+      not_in_cursor = []
+    }
   });
-  bank1Holder.addEventListener('mouseleave', function() {
 
-    //Maak kruk01 invisible
-    $(krukID01).attr('visible', 'false');
+  bank2Holder.addEventListener('mouseenter', function () {
+
+    if (in_cursor != "") {
+
+      // Maak het voorwerp zichbaar.
+      $('#' + in_cursor + '02').attr('visible', 'true');
+
+      // Haal het voorwerp uit de cursor.
+      $('#' + in_cursor + 'CursorID').attr('visible', 'false');
+
+      // Zet het in cursor variable opnieuw.
+      in_cursor = "";
+
+      $(not_in_cursor).each(function (key, value) {
+
+        // Maak het voorwerp onzichbaar.
+        $('#' + value + '02').attr('visible', 'false');
+      });
+
+      // Leeg de lijst.
+      not_in_cursor = []
+    }
   });
-  bank2Holder.addEventListener('mouseenter', function() {
 
-    //Maak kruk02 visible
-    $(krukID02).attr('visible', 'true');
+  bank3Holder.addEventListener('mouseenter', function () {
+
+    if (in_cursor != "") {
+
+      // Maak het voorwerp zichbaar.
+      $('#' + in_cursor + '03').attr('visible', 'true');
+
+      // Haal het voorwerp uit de cursor.
+      $('#' + in_cursor + 'CursorID').attr('visible', 'false');
+
+      // Zet het in cursor variable opnieuw.
+      in_cursor = "";
+
+      $(not_in_cursor).each(function (key, value) {
+
+        // Maak het voorwerp onzichbaar.
+        $('#' + value + '03').attr('visible', 'false');
+      });
+
+      // Leeg de lijst.
+      not_in_cursor = []
+    }
   });
-  bank3Holder.addEventListener('mouseenter', function() {
-
-    //Maak kruk03 visible
-    $(krukID03).attr('visible', 'true');
-  });
-
 
   // STOEL
   // Voeg een event listener toe aan de random kleur kiezer.
-  randomStoel.addEventListener('mouseenter', function() {
+  randomStoel.addEventListener('mouseenter', function () {
 
     // Verkrijg een random kleur.
     var color = getRandomColor();
@@ -121,18 +183,21 @@ $(document).ready(function () {
     $(stoel).attr('material', 'color:' + color);
   });
 
-  stoelID.addEventListener('mouseenter', function() {
+  stoelID.addEventListener('mouseenter', function () {
 
     //Maak stoelCursor visible
     $(krukCursorID).attr('visible', 'false');
     $(stoelCursorID).attr('visible', 'true');
     $(bankCursorID).attr('visible', 'false');
-  });
 
+    in_cursor = "stoel";
+
+    not_in_cursor = ["kruk", "bank"];
+  });
 
   // BANK
   // Voeg een event listener toe aan de random kleur kiezer.
-  randomBank.addEventListener('mouseenter', function() {
+  randomBank.addEventListener('mouseenter', function () {
 
     // Verkrijg een random kleur.
     var color = getRandomColor();
@@ -141,13 +206,16 @@ $(document).ready(function () {
     $(bank).attr('material', 'color:' + color);
   });
 
-  bankID.addEventListener('mouseenter', function() {
+  bankID.addEventListener('mouseenter', function () {
 
     //Maak bankCursor visible
     $(krukCursorID).attr('visible', 'false');
     $(stoelCursorID).attr('visible', 'false');
     $(bankCursorID).attr('visible', 'true');
-  });
 
+    in_cursor = "bank";
+
+    not_in_cursor = ["stoel", "kruk"];
+  });
 
 });
