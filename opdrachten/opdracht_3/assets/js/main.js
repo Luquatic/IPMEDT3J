963344d2e6419f2cc8ced6be8f32ad5f5ec35277
Luquatic <jesseyfransen@live.nl>
2016-12-07 12:00:56 +0100

    index en js

--- opdrachten/opdracht_3/assets/js/main.js
+++ opdrachten/opdracht_3/assets/js/main.js
@@ -1,3 +1,26 @@
 /**
  * Created by maartenpaauw on 07-12-16.
  */
+$(function(){
+  var data;
+  var dataString;
+
+  function getData(data)
+  {
+
+    $.ajax({
+      url: 'https://swapi.co/api' + dataString,
+      method: 'GET', // or GET
+      dataType : "json",
+      success: function(msg) {
+        var x = msg['name'] !== undefined ? msg['name'] : msg['title'];
+
+        $('#text').attr('text','text: ' + x);
+
+
+      }
+    });
+  }
+});
+
+
