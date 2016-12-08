81fa7b1eced1d639e2a99030c44418755ef3d58f
Maarten Paauw <maartenpaauw@gmail.com>
2016-12-08 12:00:41 +0100

    Basis van de huiswerkopdracht is er.

--- opdrachten/opdracht_3/assets/js/main.js
+++ opdrachten/opdracht_3/assets/js/main.js
@@ -2,10 +2,13 @@
 $(document).ready(function () {
 
   // Pokedex
-  var pokedex  = document.getElementById('pokedex-vr'),
-      nummer   = document.getElementsByClassName('nummer'),
+  var pokedex          = document.getElementById('pokedex-vr'),
+      go               = document.getElementById('go-vr'),
+      naam_vr          = document.getElementById('naam-vr'),
       totaal_nummer_vr = document.getElementById('nummer-totaal-vr'),
-      totaal_nummer = "";
+      nummer           = document.getElementsByClassName('nummer'),
+      pokemon_vr       = document.getElementById('pokemon-vr'),
+      totaal_nummer    = "";
 
   // Ga door elk nummer heen.
   for(var i = 0; i < nummer.length; i++) {
@@ -23,17 +26,61 @@ $(document).ready(function () {
     // Verkrijg het getal.
     var getal = $(nummer).attr('data-nummer');
 
-    // Toon het nummer.
-    toonNummer();
-
     // Voeg het getal toe aan de string.
     totaal_nummer += getal;
+
+    // Toon het nummer.
+    toonNummer(totaal_nummer);
   }
 
-  function toonNummer() {
+  function toonNummer(nummer) {
 
     // Toon het totale nummer.
-    $(totaal_nummer_vr).attr('text', 'text:' + totaal_nummer)
+    $(totaal_nummer_vr).attr('text', 'text:' + nummer)
+  }
+
+  goClicked();
+
+  function goClicked() {
+    go.addEventListener('mouseenter', function () {
+      if (totaal_nummer == 0) {
+        totaal_nummer = 1
+      } else  if (totaal_nummer >= 150) {
+        totaal_nummer = 150
+      }
+
+      apiCall(totaal_nummer);
+      totaal_nummer = "";
+
+      toonNummer(totaal_nummer);
+    });
+  }
+
+  function apiCall(id) {
+    $.ajax({
+      url: 'http://pokeapi.co/api/v2/pokemon/' + id + '/',
+      method: 'GET',
+      dataType: 'json',
+      success: function (msg) {
+        var afbeelding = msg.sprites.front_default;
+        var naam = msg.species.name;
+
+        toonPokemon(afbeelding, naam);
+      }
+    })
+  }
+
+  function toonNaam(naam) {
+    $(naam_vr).attr('text', 'text:' + naam)
+  }
+
+  function toonAfbeelding(afbeelding) {
+    $(pokemon_vr).attr('src', afbeelding)
+  }
+
+  function toonPokemon(afbeelding, naam) {
+    toonNaam(naam);
+    toonAfbeelding(afbeelding);
   }
 
 
