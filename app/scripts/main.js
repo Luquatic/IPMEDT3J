a14b3a6fc65ebae2138baa47e9bb4e8b7e7adbce
Maarten Paauw <maartenpaauw@gmail.com>
2016-12-09 14:47:54 +0100

    Foto's toegevoegd

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,6 @@
 // Wacht totdat het document geladen is.
 $(document).ready(function() {
-  var $nieuw_venster   = $('#nieuw-venster'),
-      $kopen           = $('#kopen-menu'),
-      $virtual_reality = $('#virtual-reality-menu');
+  var $nieuw_venster = $('#nieuw-venster');
 
   // Voeg een click event toe.
   $nieuw_venster.click(function () {
@@ -10,18 +8,4 @@ $(document).ready(function() {
     // Open de A Frame pagina in een apart tabblad.
     window.open('aframe.html');
   });
-
-  $kopen.click(function () {
-    buttonCheck(true);
-  });
-
-  $virtual_reality.click(function () {
-    buttonCheck(false);
-  });
-
-  function buttonCheck(check) {
-    if($nieuw_venster.is(':visible') == check) {
-      $nieuw_venster.toggle();
-    }
-  }
 });
