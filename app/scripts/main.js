650532558a57bb0b4b173fc08342e75f3272194f
Maarten Paauw <maartenpaauw@gmail.com>
2016-12-09 11:46:59 +0100

    Hele website

---
+++ app/scripts/main.js
@@ -0,0 +1,22 @@
+// Wacht totdat het document geladen is.
+$(document).ready(function() {
+  var $nieuw_venster   = $('#nieuw-venster'),
+      $kopen           = $('#kopen-menu'),
+      $virtual_reality = $('#virtual-reality-menu');
+
+  // Voeg een click event toe.
+  $nieuw_venster.click(function () {
+
+    // Open de A Frame pagina in een apart tabblad.
+    window.open('aframe.html');
+  });
+
+  $kopen.click(function () {
+    $nieuw_venster.toggle();
+  });
+
+  $virtual_reality.click(function () {
+    $nieuw_venster.toggle();
+  });
+
+});
