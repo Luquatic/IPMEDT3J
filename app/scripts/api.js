5f3cfa3ebd56ffd9a56ec08a5393cf60a3f9e2c6
Maarten Paauw <maartenpaauw@gmail.com>
2016-12-18 11:37:42 +0100

    API

---
+++ app/scripts/api.js
@@ -0,0 +1,55 @@
+// Kleur.
+var KLEUR = {
+
+  // API url.
+  api_url: "http://www.colr.org/json/colors/random/",
+
+  // Init functie.
+  init: function () {
+
+  },
+
+  // Alle functies.
+  functions: {
+
+    // Verkrijg een X aantal kleuren.
+    verkrijgKleuren: function(aantalKleuren) {
+
+      return new Promise(function (resolve, reject) {
+
+        // Doe een API call.
+        $.ajax({
+
+          // De URL.
+          url: KLEUR.api_url + aantalKleuren,
+
+          // Het is een GET call.
+          method: 'GET',
+
+          // We willen JSON terug krijgen.
+          dataType: 'json',
+
+          // Als de call met succes afgerond is.
+          success: function (response) {
+
+            // Doe resolve.
+            resolve(response);
+          },
+
+          // API API API toch....
+          error: function (jqXHR, textStatus, errorThrown) {
+            reject(false);
+          }
+        });
+      });
+    }
+  }
+
+};
+
+// Wacht G
+$(document).ready(function () {
+
+  // Verkrijg 6 kleuren.
+  KLEUR.functions.verkrijgKleuren(6).then(function (kleuren) {});
+});
