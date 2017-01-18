805d620b52e659754622158c303558409136de77
Maarten Paauw <maartenpaauw@gmail.com>
2017-01-18 10:55:03 +0100

    Cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,41 @@
+var HOMEPAGE = {
+
+  // jQuery
+  $: {
+
+    // nieuw venster knop.
+    knop_nieuw_venster: $('#nieuw-venster')
+  },
+
+  // Init
+  init: function () {
+
+    // Voeg de event listener toe.
+    HOMEPAGE.functions.open_in_nieuw_venster();
+  },
+
+  // Functions
+  functions: {
+
+    // Open in nieuw venster.
+    open_in_nieuw_venster: function () {
+
+      // Open de VR beleving in een nieuw venster.
+      HOMEPAGE.$.knop_nieuw_venster.click(function () {
+
+        // URL
+        var url = $(this).attr('data-link');
+
+        // Open de A Frame pagina in een apart tabblad.
+        window.open(url);
+      });
+    }
+  }
+};
+
 // Wacht totdat het document geladen is.
 $(document).ready(function() {
-  var $nieuw_venster = $('#nieuw-venster');
 
-  // Voeg een click event toe.
-  $nieuw_venster.click(function () {
-
-    // Open de A Frame pagina in een apart tabblad.
-    window.open('aframe.html');
-  });
+  // Initialiseer de homepage.
+  HOMEPAGE.init();
 });
