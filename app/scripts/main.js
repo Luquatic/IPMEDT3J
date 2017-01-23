df079abee7df2ef7076d8d74e573ac7da09b9958
Maarten Paauw <maartenpaauw@gmail.com>
2017-01-23 14:47:04 +0100

    Alvast een methode aangemaakt om de VR beleving in fullscreen te openen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,31 +4,53 @@ var HOMEPAGE = {
   $: {
 
     // nieuw venster knop.
-    knop_nieuw_venster: $('#nieuw-venster')
+    knop_nieuw_venster: $('#nieuw-venster'),
   },
+
+  // Scene
+  scene: document.querySelector('a-scene'),
 
   // Init
   init: function () {
 
     // Voeg de event listener toe.
-    HOMEPAGE.functions.open_in_nieuw_venster();
+    HOMEPAGE.functions.voeg_event_listeners_toe();
   },
 
   // Functions
   functions: {
 
-    // Open in nieuw venster.
-    open_in_nieuw_venster: function () {
+
+    // Voeg de event listeners toe.
+    voeg_event_listeners_toe: function () {
 
       // Open de VR beleving in een nieuw venster.
       HOMEPAGE.$.knop_nieuw_venster.click(function () {
 
-        // URL
-        var url = $(this).attr('data-link');
-
-        // Open de A Frame pagina in een apart tabblad.
-        window.open(url);
+        // Open in nieuw venster.
+        HOMEPAGE.functions.open_in_nieuw_venster();
       });
+    },
+
+    // Open in nieuw venster.
+    open_in_nieuw_venster: function () {
+
+      // URL
+      var url = $(this).attr('data-link');
+
+      // Open de A Frame pagina in een apart tabblad.
+      window.open(url);
+    },
+
+    // Open de VR omgeving in fullscreen.
+    open_in_full_screen: function () {
+
+      // Controleer of de VR scene geladen is.
+      if (HOMEPAGE.scene.isLoaded)
+      {
+        // Open de VR omgeving in fullscreen.
+        HOMEPAGE.scene.enterVR();
+      }
     }
   }
 };
